6a2de62802a9fdd07caca7b4fdb882dcd777dcd1
vaibhavkatoch <vaibhavkatoch04@gmail.com>
2022-04-10 23:31:14 +0530

    Bash script for jenkins project

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "Starting to deploy docker image.."
 docker pull vaibhavkatoch1/docker-image1:wordpress
-docker run -d -p 80:8080 vaibhavkatoch1/docker-image1:wordpress
+docker run -d -p 80:80 vaibhavkatoch1/docker-image1:wordpress
